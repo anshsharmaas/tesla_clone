28655d8a4670a1e75ce2afbb8bb820ab92776660
Ansh <blakemeetgain33@gmail.com>
2021-04-27 22:47:41 +0530

    inital commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import CarItem from "./components/Caritem";
+import CarsList from "./components/CarsList";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <CarsList />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    display: "flex",
+
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
